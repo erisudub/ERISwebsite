d41d1ca2db2dd1de441f7525b1c5e51a35b30593
Austin Karpf <akarpf@uw.edu>
2025-02-13 12:32:42 -0800

    Added descriptions to main page

--- app-copy.py
+++ app-copy.py
@@ -134,6 +134,34 @@ if page == "Main Page":
             unsafe_allow_html=True
         )
 
+        st.write("###What is ERIS?")
+        st.write("ERIS, which stands for Exploration and Remote Instrumentation by Students, is a student designed and built cabled observatory that serves as an underwater learning facility at the University of Washington (UW). Students work with ERIS through Ocean 462. ERIS, with its educational mission, enables undergraduate students to design, build, operate, and maintain a cabled underwater observatory that emulates the NSF")
+        st.write("Ocean Observatories Initiatives (OOI) Regional Cabled Array, by providing for a continuous data-stream for analysis, interpretation, and communication by students. From inspiration through implementation, this program is focused on the creation and operation of an underwater science sensor network that is physically located off the dock of the School of Oceanography at UW Seattle Campus.")
+        st.write("###Key Science Questions")
+        st.write("-  How do anthropogenic processes mediate natural processes in the marine environment?")
+        st.write("-  What are the temporal and spatial scales over which anthropogenic activities occur?")
+        st.write("-  How does the temperature, light, chemistry, and velocity of the marine environment change temporally and spatially?")
+        st.write("-  What unique ecological systems are present?")
+        st.write("-  What is the composition, configuration, and concentration of organisms in the different ecological systems?")
+        st.write("-  How are these systems impacted by both natural and anthropogenic events?")
+        st.write(" ")
+        st.write("ERIS will also encourage students to explore a range of technical considerations.")
+        st.write("###Technology Questions:")
+        st.write("-  What sensor(s) design is required?")
+        st.write("-  What sample rate and duty cycle is needed?")
+        st.write("-  What measurement accuracy is need and what can be achieved?")
+        st.write("-  How should remote observations be made?")
+        st.write("-  How can sensors be deployed and serviced?")
+        st.write("-  What are the power requirements?")
+        st.write("-  How will data be delivered, stored, and accessed?")
+        st.write("-  How will data be analyzed, interpreted, visualized, and communicated?")
+        st.write(" ")
+        st.write("As the observatory is being implemented, students focus on maintaining the components, as well as collecting, managing, and analyzing the continuous streams of data the observatory will produce. Integral to the ERIS program is the ability to distribute the collected data so that it may be interpreted by interested parties at the UW and worldwide.")
+
+        st.write("###Course: OCEAN 462: Ocean Technology Studio")
+        st.write("Hands-on experience to build technical, science, and management skills in ocean technology through small group projects. Projects may include instrument design and building, data analysis, and/or participation in an on-going ocean technology initiative. Offered: AWSp.  Can be taken fo 1-5 credits, with a max of 15.")
+        st.write("Visit [MyPlan](https://myplan.uw.edu/course/#/courses?states=N4Igwg9grgTgzgUwMoIIYwMYAsQC4TAA6IAZhDALYAiqALqsbkSBqhQA5RyPGJ20AbBMQA0xAJZwUGWuIgA7FOmyNaMKAjEhJASXlw1UGeSWYsjEqgGItARw0wAnkjXj5Acx4hRxACapHbjxmAEYLKxtiACZw601iAGZYyJAAFmT4kABWDK0ANgyAXy0DdFoAUXlfABVxCgQg3ABtAAYRAE48loBdLTcMAShfBAA5BQB5dgRFBBk5fVV1TP7B4YAlBtcZBF9pWQVGw2X5AaGEAAUYBCvbOA37cSvfRY0%2Bk9WEaoAjVD35w6WJSwEAA7uN5AJHOcMMhZvsFnhLHEgaDwZC9OdrnAFH8DkUUSCAEIwUGIXLELCoKRoMw7ckgXySAYQRAAQV8ADdUCcdqYVIiIghCiBCkA).")
+
         # ✅ **Auto-switch logic**
         if st.session_state.auto_switch:
             time.sleep(7)  
