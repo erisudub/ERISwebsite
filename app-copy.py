4f56e0b1614219b74dbd3abdaaf9e53f25e4cf58
Austin Karpf <akarpf@uw.edu>
2025-02-18 12:07:08 -0800

    changed to light blue

--- app-copy.py
+++ app-copy.py
@@ -25,7 +25,7 @@ st.markdown(
     <style>
         /* Change very top bar color */
         header[data-testid="stHeader"] {
-            background-color: #89CFF0 !important;  /* Baby Blue */
+            background-color: #ADD8E6 !important;  /* Light Blue */
         }
         /* Change top navigation bar color */
         section[data-testid="stHeader"] {
