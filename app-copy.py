bba4839b1395ecb242de9acf29c4fc21d1f7bd38
Austin Karpf <akarpf@uw.edu>
2025-02-27 11:58:57 -0800

    adding space for photo

--- app-copy.py
+++ app-copy.py
@@ -233,6 +233,8 @@ if page == "Main Page":
     col3, col4 = st.columns([1, 1])
 
     with col3:
+        st.write(" ")
+        st.write(" ")
         st.image("images/ctdmaintenence.jpg", use_container_width = True)
 
     with col4:
