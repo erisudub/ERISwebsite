201c263cf674e3397f40de95417d27cd85bcdcdf
Austin Karpf <akarpf@uw.edu>
2025-02-27 12:03:10 -0800

    trying

--- app-copy.py
+++ app-copy.py
@@ -233,8 +233,7 @@ if page == "Main Page":
     col3, col4 = st.columns([1, 1])
 
     with col3:
-        st.text(" ")
-        st.text(" ")
+        st.empty()
         st.image("images/ctdmaintenence.jpg", use_container_width = True)
 
     with col4:
